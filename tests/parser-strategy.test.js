bda33ef385c273971ebb61b2f93ab51d5f0ae67e
kirill.ponomarev <shacaron@gmail.com>
2017-08-24 23:09:07 +0300

    gitlab 'unknown' event = done

--- tests/parser-strategy.test.js
+++ tests/parser-strategy.test.js
@@ -3,10 +3,24 @@
 
 const should = require("should");
 const parsers = require("../parsers");
+const GitlabParserStrategy = require("../parsers/gitlab.parser-strategy");
 
 
 describe("parser-strategy", () => {
+    it("throws an error because unknown 'type' has been given", () => {
+        (function() {
+            parsers.create(null);
+        }).should.throw(/Unknown type/);
+    });
+    
+    
     describe("gitlab-parser", () => {
+        it("creates GitlatParserStrategy", () => {
+            const strategy = parsers.create(parsers.TYPES.GITLAB);
+            should(strategy).be.instanceOf(GitlabParserStrategy);
+        });
+        
+        
         describe("parse given event", () => {
             it("push", () => {
                 const mock = require("./mocks/gitlab/push-event.gitlab.json");
@@ -42,6 +56,14 @@ describe("parser-strategy", () => {
     
             it("canceled", () => {
                 const mock = require("./mocks/gitlab/build-canceled-event.gitlab.json");
+                const result = parse(parsers.TYPES.GITLAB, mock.event);
+        
+                should(result).be.eql(mock.expected);
+            });
+    
+    
+            it("returns 'null' on other cases", () => {
+                const mock = require("./mocks/gitlab/build-unknown-event.gitlab.json");
                 const result = parse(parsers.TYPES.GITLAB, mock.event);
         
                 should(result).be.eql(mock.expected);
